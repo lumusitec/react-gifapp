e386f965a63af52410d59f20ea9424e55bbed6d4
lumusitec <lumusitec@gmail.com>
2021-06-20 14:47:43 -0300

    first commit - base app

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+ 
+// Si uso React 16 - instalación oficial:
+// import Adapter from 'enzyme-adapter-react-16'; 
+ 
+// Si uso React 17 - instalación no oficial temporal:
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; 
+ 
+// Importar para mejorar el manejo de snapShots
+import { createSerializer } from 'enzyme-to-json'
+ 
+Enzyme.configure({ adapter: new Adapter() });
+ 
+// Para mejorar el manejo de snapShots
+expect.addSnapshotSerializer( createSerializer({ mode: 'deep' }) );
